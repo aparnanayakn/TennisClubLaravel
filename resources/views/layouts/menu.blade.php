b2162190afbe1a38cfe7276ee9ef652bf4a9334f
aparnanayakn <naparnanayak@gmail.com>
2026-03-22 09:34:04 +0000

    base template

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,24 @@
+<li class="nav-item">
+    <a href="{{ route('members.index') }}"
+       class="nav-link {{ Request::is('members*') ? 'active' : '' }}">
+        <p>Members</p>
+    </a>
+</li>
+
+
+<li class="nav-item">
+    <a href="{{ route('courts.index') }}"
+       class="nav-link {{ Request::is('courts*') ? 'active' : '' }}">
+        <p>Courts</p>
+    </a>
+</li>
+
+
+<li class="nav-item">
+    <a href="{{ route('bookings.index') }}"
+       class="nav-link {{ Request::is('bookings*') ? 'active' : '' }}">
+        <p>Bookings</p>
+    </a>
+</li>
+
+
