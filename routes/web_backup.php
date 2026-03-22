541f404d91ffd51c9059e4d7a76dca9dd72ab486
aparnanayakn <naparnanayak@gmail.com>
2026-03-22 14:34:10 +0000

    breeze

--- routes/web_backup.php
+++ routes/web_backup.php
@@ -17,12 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
-
 
 Route::resource('members', App\Http\Controllers\memberController::class);
 
